6a952999a4c87c268078e7f15933284d166a7730
XULONG LIU <leoliuxulong@hotmail.com>
2018-01-10 20:24:30 -0400

    2018/1/10 C++武器类初始化测试

--- Source/WinterBattle/WBWeapon.cpp
+++ Source/WinterBattle/WBWeapon.cpp
@@ -8,7 +8,8 @@ AWBWeapon::AWBWeapon()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	// Set WeaponMesh 
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
 }
 
 // Called when the game starts or when spawned
--- Source/WinterBattle/WBWeapon.h
+++ Source/WinterBattle/WBWeapon.h
@@ -15,14 +15,13 @@ public:
 	// Sets default values for this actor's properties
 	AWBWeapon();
 
-protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
+	// Sets a weapon mesh 
+	UStaticMeshComponent* WeaponMesh;
 	
 };
